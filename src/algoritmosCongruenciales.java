3dd7573fd71f0c73f58c416a996fa09ed70df1e9
Antonio <isanfloresfa@ittepic.edu.mx>
2022-03-21 13:02:58 -0700

    add files

---
+++ src/algoritmosCongruenciales.java
@@ -0,0 +1,21 @@
+
+public class algoritmosCongruenciales {
+  private int cantidad;
+
+  public algoritmosCongruenciales(int cant){
+    this.cantidad = cant;
+  }
+
+  public float[] AlgoritmoLineal(){
+    float[] numGenerados = new float[cantidad];
+
+    return numGenerados;
+  }
+
+  public float[] AlgoritmoMultiplicativo(){
+    float[] numGenerados = new float[cantidad];
+
+    return numGenerados;
+  }
+
+}
